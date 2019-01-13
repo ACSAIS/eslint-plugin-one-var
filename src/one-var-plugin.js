15dbcee24358721e9f80475327b8af6f16c7f653
Georgy Klyukovkin <georgy.klyukovkin@nordigy.ru>
2019-01-14 00:02:43 +0300

    need to write ast into file

--- src/one-var-plugin.js
+++ src/one-var-plugin.js
@@ -1,11 +1,12 @@
 import espree from "espree";
-
+import traverse from "babel-traverse";
+import fs from "fs";
 
 
 export function fixer(code) {
-    console.log(code);
     const ast = espree.parse(code, {
         ecmaVersion: 6,
     });
-    console.log(ast);
+    console.log(typeof ast);
+    fs.writeFileSync('src/result', ast.toString());
 }
