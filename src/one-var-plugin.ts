5a44b956b9feb2c06a3c5e165f565c5960811ca5
ACSAIS <warlack@outlook.com>
2019-01-14 02:21:16 +0300

    nothing works, we don't give up

--- src/one-var-plugin.ts
+++ src/one-var-plugin.ts
@@ -1,22 +1,42 @@
 import * as espree from "espree";
 import traverse from "babel-traverse";
 import * as fs from "fs";
+import generate from "babel-generator";
+import * as t from "babel-types";
 
 export function fixer(code) {
     const ast = espree.parse(code, {
         ecmaVersion: 6,
     });
-
-    fs.writeFileSync('src/result.json', JSON.stringify(ast,null, 4), 'utf8');
     traverse(ast, {
         enter: function(path) {
             if (path.node.type === "VariableDeclaration") {
-                //path.replaceWith(replacement)
                 if (path.node.declarations.length > 1) {
                     const {declarations} = path.node;
-                    // console.log(JSON.stringify(path.node.kind,null, 4),);
+                    const firstDeclaration = declarations.shift();
+                    path.node.declarations = [firstDeclaration];
+                    const newNodes = [];
+                    declarations.forEach( declaration => {
+                        let newNode = {};
+                        require("ast-types").eachField(path.node, function(name, value) {
+                            // Note that undefined fields will be visited too, according to
+                            // the rules associated with node.type, and default field values
+                            // will be substituted if appropriate.
+                            newNode[name] = value;
+                        });
+
+                        newNode.declarations = [declaration];
+                        newNodes.push(newNode);
+                        console.log(path.node);
+                        path.body.insertAfter(t.identifier("b"));
+                    });
+                    // console.log(newNodes);
+                    // path.insertAfter(newNodes);
                 }
             }
         },
     });
+    const generated = generate(ast, {}, code);
+    fs.writeFileSync('src/result.js', generated.code);
+
 }
